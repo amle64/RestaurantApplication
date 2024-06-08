3314c56330ea5a8dc503874a866c8f645696ece1
Andrew Le <andrew.le.careers@gmail.com>
2024-06-08 16:25:16 -0500

    Initial commit and created FoodMenu interface for food options

---
+++ src/Menu/FoodMenu.java
@@ -0,0 +1,11 @@
+package Menu;
+
+public interface FoodMenu {
+    public void getDescription();
+
+    public void getFlavorProfile();
+
+    public void getPrice();
+
+    public void getFoodType();
+}
